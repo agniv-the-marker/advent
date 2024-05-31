46b3b90884aa1e0f62c2a065716c2cc29e712418
Agniv Sarkar <agnivsarkar@proofschool.org>
2024-05-31 10:02:24 -0700

    Need to Redo problem 5.2, fixed rust input for 5.1

--- rust/src/fertilizer.rs
+++ rust/src/fertilizer.rs
@@ -1,63 +1,82 @@
-use std::fs::File;
-use std::io::{self, BufRead};
+use std::fs::read_to_string;
+use std::io;
 
-fn short_order(data: Vec<String>) -> i64 { // Broken right now because of faulty data formatting
+fn short_order(data: Vec<String>) -> i64 {
     let mut orders: Vec<i64> = data[0][7..].split_whitespace().map(|s| s.parse().unwrap()).collect();
     let maps = &data[1..];
     for almanac in maps {
-        if almanac.is_empty() || !almanac.chars().next().unwrap().is_numeric() { continue }
-        let range: Vec<i64> = almanac.split(' ').map(|x| x.parse().unwrap()).collect();
-        let (dest_start, source_start, range_len) = (range[0], range[1], range[2]);
+        let ranges: Vec<Vec<i64>> = almanac.split('\n').skip(1).map(|triple| triple.split_whitespace().map(|s| s.parse().unwrap()).collect()).collect();
         for order in orders.iter_mut() {
-            if *order >= source_start && *order < source_start + range_len {
-                *order = dest_start + *order - source_start;
+            for range in &ranges {
+                let (dest_start, source_start, range_len) = (range[0], range[1], range[2]);
+                if source_start <= *order && *order < source_start + range_len {
+                    *order = dest_start + *order - source_start;
+                    break // do not repeat mappings
+                }
             }
         }
     }
-    *orders.iter().min().unwrap()
+    let ans = orders.iter().min().unwrap();
+    *ans
 }
 
 fn range_order(data: Vec<String>) -> i64 {
-    let orders: Vec<i64> = data[0][7..].split_whitespace().map(|s| s.parse().unwrap()).collect();
-    let mut inputs: Vec<(i64, i64)> = orders.chunks(2).map(|chunk| (chunk[0], chunk[1])).collect();
-    for mapping in &data[1..] {
-        let mut new_inputs = Vec::new();
-        for (start, length) in inputs {
-            let mut start = start;
-            let mut length = length;
-            while length > 0 {
-                let mut found = false;
-                for triple in mapping.split('\n').skip(1) {
-                    let parts: Vec<i64> = triple.split_whitespace().map(|s| s.parse().unwrap()).collect();
-                    let (dest_start, source_start, range_len) = (parts[0], parts[1], parts[2]);
-                    let delta = start - source_start;
-                    if delta >= 0 && delta < range_len {
-                        let new_interval_length = std::cmp::min(range_len - delta, length);
-                        new_inputs.push((dest_start + delta, new_interval_length));
-                        start += new_interval_length;
-                        length -= new_interval_length;
-                        found = true;
-                        break;
-                    }
-                }
-                if !found {
-                    new_inputs.push((start, length));
-                    break;
-                }
-            }
-        }
-        inputs = new_inputs;
-    }
-    inputs.iter().map(|(start, _)| *start).min().unwrap()
+    data[0][7..].split_whitespace().map(|s| s.parse::<i64>().unwrap()).sum()
 }
 
 pub fn solve(input_directory: &str, file_name: &str) -> io::Result<()> {
     let file_path = format!("{}{}", input_directory, file_name);
-    let file = File::open(file_path)?;
-    let reader = io::BufReader::new(file);
-    
-    let file_data: Vec<String> = reader.lines().map(|line| line.unwrap()).collect(); // TODO: fix to do split
-    println!("task 1: {}", short_order(file_data.clone()));
-    println!("task 2: {}", range_order(file_data));
+    let read = read_to_string(file_path)?;
+    let data: Vec<String> = read.split("\n\n").map(|s| s.to_string()).collect();
+
+    println!("task 1: {}", short_order(data.clone()));
+    println!("task 2: {}", range_order(data));
+
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const DATA: &str = "seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4";
+
+    #[test]
+    fn test_short_order() {
+        let data = DATA.split("\n\n").map(|s| s.to_string()).collect();
+        let result = short_order(data);
+        assert_eq!(result, 35);
+    }
 }
